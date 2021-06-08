1f162203fefb8e902a694f1ef289038a0dee7bf9
DhilipThangaraj <DhilipThangaraj777@gmail.com>
2021-06-08 09:53:30 +0530

    express configuration

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello world.Keep rock it.");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) =>
+  res.send({
+    message: "Api is running",
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
