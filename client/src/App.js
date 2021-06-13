f30a4f3430e863ffe6ced3cbcbc8fcc12521e832
DhilipThangaraj <DhilipThangaraj777@gmail.com>
2021-06-13 15:16:51 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from "react";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+      </Fragment>
+    </Router>
   );
 }
 
