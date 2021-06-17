b19cfbf21a34df5a4be737df6b86454c8f1de121
DhilipThangaraj <DhilipThangaraj777@gmail.com>
2021-06-18 05:06:09 +0530

    profile action has been created

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,5 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOG_OUT = "LOG_OUT";
+export const GET_PROFIE = "GET_PROFIE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
