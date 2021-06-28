93fb2ccfb4a0b7468ec12dca23e3c267788042ae
dhilipthangaraj <dhilipthangaraj777@gmail.com>
2021-06-28 12:13:06 +0530

    added edit profile functionality

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 import Dashboard from "../src/components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./routing/PrivateRoute";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -41,6 +42,11 @@ function App() {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
               />
             </Switch>
           </section>
