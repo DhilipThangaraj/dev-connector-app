95eb0586e2715ca50c24ad97f0ea4227dfe5544f
dhilipthangaraj <dhilipthangaraj777@gmail.com>
2021-07-02 09:41:41 +0530

    pushing the code for displaying the profile

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 import PrivateRoute from "./routing/PrivateRoute";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -39,6 +40,7 @@ function App() {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
